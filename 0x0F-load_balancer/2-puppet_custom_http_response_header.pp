f5ea333bfe72705acdd04892d4ebb9bbdd774b74
delaney2023 <alxse2@gmail.com>
2023-10-07 01:28:12 -0700

    a custom HTTP header response, but with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -11,3 +11,46 @@
 package { 'nginx':
   ensure => installed,
 }
+
+file { '/var/www/html':
+  ensure => directory,
+}
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!',
+}
+
+file { '/var/www/html/404.html':
+  ensure  => present,
+  content => "Ceci n'est pas une page",
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => '
+    server {
+      listen 80 default_server;
+      listen [::]:80 default_server;
+      add_header X-Served-By $hostname;
+      root /var/www/html;
+      index index.html index.htm;
+
+      location /redirect_me {
+        return 301 https//youtube.com/;
+      }
+
+      error_page 404 /404.html;
+      location /404 {
+        root /var/www/html;
+        internal;
+      }
+    }
+  ',
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/nginx/sites-available/default'],
+}
